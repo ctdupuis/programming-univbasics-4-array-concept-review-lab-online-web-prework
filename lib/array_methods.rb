6f595c8a912c1e5a57c33c2cf834c97084b4e303
Cody Dupuis <ctdupuis7@gmail.com>
2019-08-23 02:49:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,12 +20,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  x = array[0]
-  array.length.times { |index|
+ x = array[0]
+ array.length.times { |index|
   if array[index] < x 
     x = array[index]
-  end
-  }
-  return x 
+ }
+ x 
 end
 end
